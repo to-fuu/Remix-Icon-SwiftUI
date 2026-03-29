a5a4a90ff314ffd4d052198ab6b0fd31997db3dc
to-fuu <chebbi.ala@outlook.com>
2026-03-29 14:35:09 +0900

    v4.9.1

--- Sources/Remix/Remix.swift
+++ Sources/Remix/Remix.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+extension ImageAsset {
+    @available(iOS 13.0, *)
+    func asIcon(_ size: CGFloat = 18.0) -> some View {
+        self.swiftUIImage
+            .renderingMode(.template)
+            .resizable().scaledToFit().frame(width: size, height: size)
+            .foregroundStyle(.primary)
+    }
+}
+
+#Preview {
+    VStack {
+        RemixIcons.Document.receiptLine.asIcon()
+    }
+}
